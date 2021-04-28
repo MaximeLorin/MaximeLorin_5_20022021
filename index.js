7a1d4911085c18710a64f3e79ce9a1fe068f82db
MaximeLorin <maxime.lorin56@gmail.com>
2021-04-28 12:09:53 +0200

    Index page javascript is done

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-let image= document.getElementById("article__img");
-let price= document.getElementById("article__price");
-let description= document.getElementById("article__descrptn");
-let content= document.getElementById("content");
-
-let listdata={
-    Lenses:[],
-    _id:0,
-    name:"",
-    description:"",
-    price:0,
-    imageUrl:"",
-};
-
-
+class objArticle{
+    constructor(description,imageUrl,Lenses,name,price,_id){
+        this.Lenses= [];
+        this._id=_id;
+        this.name=name;
+        this.description=description;
+        this.price=0;
+        this.imageUrl=imageUrl;
+    }
+}
 
 getElements();
 
@@ -21,43 +16,45 @@ function createArticles()
     const newArticle =document.createElement("div");
     newArticle.id= "article";
     newArticle.classList.add("article");
-
+    
     const newImage= document.createElement("img");
     newImage.classList.add("article__img");
     newImage.id= "article__img";
+    newImage.src= objArticle.imageUrl;
 
     const newPrice= document.createElement("h2");
     newPrice.classList.add("article__price");
     newPrice.id= "article__price";
+    newPrice.textContent = objArticle.price/100+" €";
+
+    const newName= document.createElement("h3");
+    newName.classList.add("article__name");
+    newName.id= "article__name";
+    newName.textContent=objArticle.name;
 
     const newDescription= document.createElement("p");
     newDescription.classList.add("article__descrptn");
     newDescription.id= "article__descrptn";
+    newDescription.textContent = objArticle.description;
 
     newArticle.append(newImage);
     newArticle.append(newPrice);
+    newArticle.append(newName);
     newArticle.append(newDescription);
     
     document.getElementById("content").appendChild(newArticle);
-    
-    price.textContent = listdata.price/100+" €";
 }
 
 function getElements(){
     fetch ("http://localhost:3000/api/cameras")
         .then(response =>response.json())
         .then(data =>{
-            console.log(data)
             for (let article in data){
-                listdata=data[article];
-                createArticles();
-                image.src= listdata.imageUrl;
-                
-                description.textContent = listdata.description;
-                console.log(listdata)
+                objArticle=data[article];
+                createArticles();               
             }
-            
         })
+        .catch(error => alert(error))
 } 
 
 
